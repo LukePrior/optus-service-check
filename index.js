f81a699d0696f3d66a67e1ae0e517c5d5cfd96c2
Uskompuf <22492406+Uskompuf@users.noreply.github.com>
2022-01-26 20:43:47 +1100

    test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
+var express = require("express");
+var app = express();
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
 
 app.get("/url", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
